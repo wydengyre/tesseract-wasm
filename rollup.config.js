40d6f90a438973e1fe7ee53029af1134ab0f8d08
Matt Leon <108271225+wydengyre@users.noreply.github.com>
2024-05-13 09:53:54 +0700

    kill ocr client: support only ocr engine (#25)
    We also kill examples: tests can cover their usage.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,6 @@
-import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 
 export default [
-  {
-    input: "src/worker.ts",
-    output: {
-      dir: "dist",
-      entryFileNames: "tesseract-worker.js",
-
-      // nb. Compiled as a classic script because Firefox and Safari < 15 do
-      // not support module workers. See https://caniuse.com/mdn-api_worker_worker_ecmascript_modules.
-      format: "umd",
-    },
-    plugins: [
-      typescript(),
-      nodeResolve(),
-    ],
-  },
   {
     input: "src/index.ts",
     output: {
@@ -26,8 +10,6 @@ export default [
     },
     plugins: [
       typescript(),
-      nodeResolve(),
     ],
   },
-  
 ];
